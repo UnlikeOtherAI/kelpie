e36d19985ff5fe7a288746097b8aa4a8e0110107
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2026-06-13 09:55:13 +0100

    fix: accept public device info posts

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.kelpie.browser"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.1"
+        versionCode = 2
+        versionName = "0.1.2"
 
         externalNativeBuild {
             cmake {
